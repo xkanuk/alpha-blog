bd81f3f7f95958372ac9ddde5e921897de55a453
Sherman Simpson <xkanuk@gmail.com>
2020-04-13 20:48:35 +1000

    Add Show and Delete actions to index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,7 +36,12 @@ class ArticlesController < ApplicationController
     else 
       render 'edit'
     end
+  end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
 end
