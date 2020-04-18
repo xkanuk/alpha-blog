eb644014661ed880f7746d708f396cd015da5a45
Sherman Simpson <xkanuk@gmail.com>
2020-04-18 17:33:57 +1000

    Fix email validation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-playing with bootstrap
+Validations for User class:
+
+- username must be present and unique, length between 3 and 25 characters
+
+- email must be present and unique, length max of 105 characters
+
+- validate email format using regex (regular expression)
+The ruby regular expression used to match the format of valid email addresses is listed below.
+
+/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   validates :username, presence: true, 
                       uniqueness: { case_sensitive: false }, 
                       length: { minimum: 3, maximum: 25 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[-\w.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
                       uniqueness: { case_sensitive: false }, 
                       length: { maximum: 105 },
