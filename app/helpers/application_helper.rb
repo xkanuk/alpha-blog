8c18479440cfff0f0cf715308ec899aeca36faa9
Sherman Simpson <xkanuk@gmail.com>
2020-05-03 13:29:21 +1000

    moved current_user and logged_in? methods to controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,15 +8,6 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
 
-  def current_user
-    @current_user || User.find(session[:user_id]) if session[:user_id]
-
-  end
-
-  def logged_in?
-    !!current_user
-  end
-
   def require_user
     if !logged_in?
       flash[:danger] = "You must be logged in to perform that action"
